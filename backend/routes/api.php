e0863b8d5834a378d86af8006abbf8f6b9f0575f
Hadbaazokri12 <ha.zokri@gmail.com>
2026-09-11 18:33:01 +0100

    SCRUM-629: generer les donnees de l'ordonnance imprimable

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -172,6 +172,8 @@ Route::middleware('auth:sanctum')->group(function () {
         ->middleware('can:ordonnances.read');
     Route::get('/ordonnances/{id}', [OrdonnanceController::class, 'show'])
         ->middleware('can:ordonnances.read');
+    Route::get('/ordonnances/{id}/imprimer', [OrdonnanceController::class, 'imprimer'])
+        ->middleware('can:ordonnances.read');
     Route::post('/ordonnances', [OrdonnanceController::class, 'store'])
         ->middleware('can:ordonnances.create');
     Route::put('/ordonnances/{id}', [OrdonnanceController::class, 'update'])
